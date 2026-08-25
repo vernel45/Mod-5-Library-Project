eec39f419174f26d0de6b7c420684257f4840706
Sam Verneuille <samvernel268@gmail.com>
2026-08-25 11:24:25 -0400

    updated

--- books.js
+++ books.js
@@ -1,74 +1,20 @@
-let books;
-
-async function renderBooks(filter) {
+function renderBooks() {
   const booksWrapper = document.querySelector(".books");
-
-  booksWrapper.classList += ' books__loading'
-
-  if (!books) {
-    books = await getBooks();
-  }
-  
-  booksWrapper.classList.remove('books__loading')
-
-  if (filter === "LOW_TO_HIGH") {
-    books.sort(
-      (a, b) =>
-        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
-    );
-  } else if (filter === "HIGH_TO_LOW") {
-    books.sort(
-      (a, b) =>
-        (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)
-    );
-  } else if (filter === "RATING") {
-    books.sort((a, b) => b.rating - a.rating);
-  }
-
-  const booksHtml = books
-    .map((book) => {
-      return `<div class="book">
-    <figure class="book__img--wrapper">
-      <img class="book__img" src="${book.url}" alt="">
-    </figure>
-    <div class="book__title">
-      ${book.title}
-    </div>
-    <div class="book__ratings">
-      ${ratingsHTML(book.rating)}
-    </div>
-    <div class="book__price">
-      ${priceHTML(book.originalPrice, book.salePrice)}
-    </div>
-  </div>`;
-    })
-    .join("");
-
-  booksWrapper.innerHTML = booksHtml;
-}
-
-function priceHTML(originalPrice, salePrice) {
-  if (!salePrice) {
-    return `$${originalPrice.toFixed(2)}`;
-  }
-  return `<span class="book__price--normal">$${originalPrice.toFixed(
-    2
-  )}</span>$${salePrice.toFixed(2)}`;
-}
-
-function ratingsHTML(rating) {
-  let ratingHTML = "";
-  for (let i = 0; i < Math.floor(rating); ++i) {
-    ratingHTML += '<i class="fas fa-star"></i>\n';
-  }
-  if (!Number.isInteger(rating)) {
-    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
-  }
-  return ratingHTML;
-}
-
-function filterBooks(event) {
-  renderBooks(event.target.value);
+  console.log(booksWrapper);
+  booksWrapper.innerHTML = `<div class="book__title">
+Crack the Coding Interview
+</div>
+<div class="book__ratings">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star-half-alt"></i>
+</div>
+<div class="book__price">
+<span class="book__price--normal">$59.95</span> $14.95
+</div>
+</div>`;
 }
 
 setTimeout(() => {
